8653656798ef1c4a1afeefc8b45d1e54fbac90c5
Matheus-Carvalho1 <48157146+Matheus-Carvalho1@users.noreply.github.com>
2019-06-08 21:51:59 -0300

    Agora sim com comentarios kk

--- Java/tratamentoMatriz/transformaMatriz_pt1.java
+++ Java/tratamentoMatriz/transformaMatriz_pt1.java
@@ -1,37 +1,37 @@
-import java.util.ArrayList;
+import java.util.ArrayList; //puxa as listas em java
 public class transformaMatriz_pt1 {
     static String[][] tira_repetida(String Matriz[][]) { //FUNCIONANDO
-        String[][] aux;
+        String[][] aux;  //matriz que sera retornada
         int contador = 0;
         int corretor;
-        ArrayList<Integer> linhas = new ArrayList<Integer>();
-        boolean igual = true;
-        for (int i = 1; i < Matriz.length - 1; i++) {
-            for (int j = i + 1; j < Matriz.length; j++) {
-                for (int k = 0; k < Matriz[0].length - 1; k++) {
+        ArrayList<Integer> linhas = new ArrayList<Integer>(); //lista que recebera o numero das linhas repetidas
+        boolean igual = true; // vamos supor que as linhas sao iguais
+        for (int i = 1; i < Matriz.length - 1; i++) { //percorre a matriz ate a penultima linha
+            for (int j = i + 1; j < Matriz.length; j++) { //percorre da linha "i" ate a ultima
+                for (int k = 0; k < Matriz[0].length - 1; k++) { //compara os elementos da linha
                     if (Matriz[i][k].equals(Matriz[j][k]) == false) {
-                        igual = false;
+                        igual = false; //se tiver diferenca muda para false
                     }
                 }
-                if (igual) {
+                if (igual) { //se continuar igual entra no contador de linhas iguais e adiciona o indice da linha na lista
                     contador++;
                     linhas.add(j);
                 }
                 igual = true;
             }
             while (!linhas.isEmpty()) {
-                if (!Matriz[i][Matriz[0].length - 1].contains(Matriz[linhas.get(0)][Matriz[0].length - 1])) {
+                if (!Matriz[i][Matriz[0].length - 1].contains(Matriz[linhas.get(0)][Matriz[0].length - 1])) { //se nao tiver listado a doenca adiciona
                     Matriz[i][Matriz[0].length - 1] = Matriz[i][Matriz[0].length - 1].concat(" ");
                     Matriz[i][Matriz[0].length - 1] = Matriz[i][Matriz[0].length - 1].concat(Matriz[linhas.get(0)][Matriz[0].length - 1]);
                 }
-                Matriz[linhas.get(0)][0] = "Sou igual";
+                Matriz[linhas.get(0)][0] = "Sou igual"; //marca a linha para depois ser removida
                 linhas.remove(0);
             }
         }
-        aux = new String[Matriz.length - contador][Matriz[0].length];
-        corretor = 0;
-        for (int i = 0; i < Matriz.length; i++) {
-            if (!Matriz[i][0].contains("Sou igual")) {
+        aux = new String[Matriz.length - contador][Matriz[0].length]; //cria uma matriz com o numero de linhas sem as repetidas
+        corretor = 0; //acertar a copia
+        for (int i = 0; i < Matriz.length; i++) {  //copia
+            if (!Matriz[i][0].contains("Sou igual")) { //se tiver marcada nao copia
                 for (int j = 0; j < Matriz[0].length; j++) {
                     aux[i - corretor][j] = Matriz[i][j];
                 }
@@ -39,10 +39,10 @@ public class transformaMatriz_pt1 {
                 corretor++;
             }
         }
-        return aux;
+        return aux; // retorna a matriz
     }
 
-    static void printArray(String Matriz[][]) { //FUNCIONANDO
+    static void printArray(String Matriz[][]) { //FUNCIONANDO imprime a matriz
         int n = Matriz.length;
         int m = Matriz[0].length;
         for (int i = 0; i < n; i++) {
@@ -56,14 +56,14 @@ public class transformaMatriz_pt1 {
     static int num_sintomas(String[][] matriz, String doenca) {
         int contador = 0;
         int sintomas = -1;
-        for (int i = 0; i < matriz.length; i++) {
-            if (matriz[i][matriz[0].length - 1].contains(doenca)) {
-                for (int j = 0; j < matriz[0].length - 1; j++) {
-                    if (matriz[i][j].contains("t")) {
+        for (int i = 0; i < matriz.length; i++) { //percorre a coluna das doencas
+            if (matriz[i][matriz[0].length - 1].contains(doenca)) { // se achar a doenca 
+                for (int j = 0; j < matriz[0].length - 1; j++) { 
+                    if (matriz[i][j].contains("t")) { // analisa o numero de sintomas
                         contador++;
                     }
                 }
-                if (contador > sintomas) {
+                if (contador > sintomas) { // se achar uma linha com mais sintomas, substitui para devolver o maximo
                     sintomas = contador;
                 }
             }
@@ -74,14 +74,14 @@ public class transformaMatriz_pt1 {
     static int doencas_possiveis(String[][] Matriz, String sintoma) {
         int contador = 0;
         int doencas = -1;
-        for (int i = 0; i < Matriz[0].length; i++) {
-            if (Matriz[0][i].contains(sintoma)) {
+        for (int i = 0; i < Matriz[0].length; i++) { //percorre a linha dos sintomas
+            if (Matriz[0][i].contains(sintoma)) { //se achar o sintoma
                 for (int j = 1; j < Matriz.length; j++) {
-                    if (Matriz[j][i].contains("t")) {
+                    if (Matriz[j][i].contains("t")) { // ve quantas doencas tem esse sintoma
                         contador++;
                     }
                 }
-                if (contador > doencas) {
+                if (contador > doencas) { // se achar uma coluna com mais doencas, substitui para devolver o maximo
                     doencas = contador;
                 }
             }
